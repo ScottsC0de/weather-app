c3e777da53a5cba1b3852d27c1426f4716815cf1
scottsc0de <scott5902@gmail.com>
2022-10-24 16:22:47 -0400

    cleaned and commented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,12 @@
 // API request by city name
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
-// module api link
+// Module API link
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-// NEEDS https://
-// weatherApiKey = "https://api.openweathermap.org/data/2.5/forecast?q=hartford&appid=43eade946f6708f970e3b3d38a9999a2";
-// console.log(fetch(weatherApiKey));
+// Geocoding API
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-// targetting html elements
 var currentCity = document.getElementById('current-city');
 var fiveDay = document.getElementById('five-day');
 var searchBtn = document.getElementById('search-button');
@@ -21,11 +19,6 @@ var windSpeedDisplay = document.getElementById('wind-speed');
 var currentHumidityDisplay = document.getElementById('humidity');
 var recentSearches = document.getElementById('recent-searches');
 
-var searchHistory = []; // push user search value into array that will be put into LS
-
-// geocoding API
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
 // 2 api calls to grab data
 function getLatLon(city) {
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ", us&appid=43eade946f6708f970e3b3d38a9999a2"
@@ -33,7 +26,7 @@ function getLatLon(city) {
         .then(function (response) {
             return response.json();
         })
-        // response = array of object sent
+
         .then(function (response) {
             console.log(response);
             var cityLat = response[0].lat;
@@ -57,20 +50,18 @@ function getForecast(lat, lon) {
             var cityState = response.city.country; // change to state somehow
             currentCityName.textContent = cityName + ", " + cityState;
 
-            // var currentDate = response.list[0].dt_txt;
-            // convert to normal looking time
             var currentDate = moment().format('dddd MMM Do, YYYY');
             todaysDateDisplay.textContent = currentDate;
 
             var cityIcon = response.list[0].weather[0].icon;
             var cityIconConvert = document.createElement('img');
             cityIconConvert.src =
-                "https://openweathermap.org/img/w/" + cityIcon + ".png"; // get icon to display
+                "https://openweathermap.org/img/w/" + cityIcon + ".png";
             currentCity.appendChild(cityIconConvert);
             clearOldIcon(cityIconConvert, cityIconConvert);
 
             var cityTemp = response.list[0].main.temp;
-            cityTempConvert = Math.floor((cityTemp - 273.15) * 9 / 5 + 32)// (K − 273.15) × 9/5 + 32 = °F. Kelvins to Farenheit
+            cityTempConvert = Math.floor((cityTemp - 273.15) * 9 / 5 + 32) // (K − 273.15) × 9/5 + 32 = °F. Kelvins to Farenheit
             cityTempDisplay.textContent = "Temp: " + cityTempConvert + "°F"; // shift+option+8 for degree symbol (Mac)
 
             var windSpeed = response.list[0].wind.speed;
@@ -79,8 +70,8 @@ function getForecast(lat, lon) {
             var cityHumidity = response.list[0].main.humidity;
             currentHumidityDisplay.textContent = "Humidity: " + cityHumidity + "%";
 
+            // old data gets replaced with new data every search
             fiveDay.textContent = "";
-
 
             // for loop to get 5 day
             for (i = 0; i < 6; i++) {
@@ -88,7 +79,7 @@ function getForecast(lat, lon) {
                 var dayContainers = document.createElement('div');
                 dayContainers.classList.add("col", "bg-light", "mr-3", "text-center");
 
-                // weather data was every 3 hours, need to convert to 5 day
+                // weather data was every 3 hours, needed to convert to 5 day
                 indexConvert = i * 8 + 4;
 
                 var fiveDayDate = document.createElement("p")
@@ -113,35 +104,31 @@ function getForecast(lat, lon) {
                 dayContainers.appendChild(fiveDayWind);
 
                 fiveDay.appendChild(dayContainers);
-                // clearOldData();
             }
-
-
         });
 }
 
-// button function, onclick, api is called for current city and 5 day weather
 searchBtn.addEventListener('click', function (e) {
     e.preventDefault();
     var userCity = userInput.value;
     getLatLon(userCity);
     saveSearch(userCity);
+    var searchHistory = [];
+    searchHistory.push(userCity);
+    localStorage.setItem(searchHistory, JSON.stringify(searchHistory));
 });
 
 function saveSearch(city) {
     var recentCity = document.createElement('button');
+    var spaceBetween = document.createElement('br');
     recentCity.textContent = city;
     recentSearches.appendChild(recentCity);
-    // clearRecents();
-    // saves user recent searches to page
-    // local storage
-    // display on page and have it stay there   
-}
+    recentSearches.appendChild(spaceBetween);
+    // displayRecents(recentCity);
+};
 
 function clearOldIcon(newIcon, oldIcon) {
     weatherIconDisplay.replaceChildren(newIcon, oldIcon);
 };
 
-// function clearOldData(newData, oldData) { fiveDay.replaceChildren() }
-
-// need function when you click recent city, runs search button function
+ // clearRecents(); button
